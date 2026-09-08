0f9e1f0bf25eabddf206638336511c4027749e04
rakeshk110 <rakeshrh6767@gmail.com>
2026-09-08 19:56:20 +0530

    Initial commit

---
+++ basics.py
@@ -0,0 +1,46 @@
+"""
+name = "Python"
+ph = 1651521854151
+status = True
+active = "False"
+print(name)
+print(ph)
+print(status)
+print(type(active)) """
+
+#input function
+
+# user_input = input("Enter Name :")
+# print(user_input)
+
+#conditional statement
+age = 15
+
+# if age >= 18:
+#     print("Eligible to vote")
+# else:
+#     print("Not eligible to vote") 
+
+#Loops
+
+# for i in range(1,6):
+#     print("python")
+
+# num = 1
+# while num<=5:
+#     print("Python")
+#     num = num+1
+
+#Function 
+
+def cals(a,b):
+    return a+b
+
+#print(cals(10,10))
+cals(5,10)
+cals(50,50)
+
+
+num = [10,20,30,40,"python"]
+
+print(num[2:5])
