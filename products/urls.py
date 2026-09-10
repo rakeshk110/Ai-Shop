a01ae6a150b116f2fc0565655863a3a95b4f8e13
rakeshk110 <rakeshrh6767@gmail.com>
2026-09-10 20:05:00 +0530

    day 7

--- products/urls.py
+++ products/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
-from .views import home
+from .views import home,category_products
 
 urlpatterns = [
     path("home/",home),
+    path("category/<int:category_id>/", category_products,name="category_products")
+    
 ]
--- products/views.py
+++ products/views.py
@@ -1,4 +1,20 @@
-from django.shortcuts import render
+from django.shortcuts import render,get_object_or_404
+from .models import Product,Category
 
 def home(request):
-    return render(request,"home.html")
+    products = Product.objects.all()
+    categories = Category.objects.all()
+    return render(request,"home.html",{
+        "products":products,
+        "categories": categories
+        })
+
+def category_products(request,category_id):
+    category = get_object_or_404(Category,id=category_id)
+    products = Product.objects.filter(Category=category)
+    categories = Category.objects.all()
+    return render(request,"home.html",{
+            "products":products,
+            "categories": categories
+            })
+
