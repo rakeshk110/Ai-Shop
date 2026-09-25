0b27de5519f8935d0dd33efb1713afa0653b3358
rakeshk110 <rakeshrh6767@gmail.com>
2026-09-25 19:40:40 +0530

    Ready to deployment

--- config/settings.py
+++ config/settings.py
@@ -2,6 +2,7 @@
 
 from pathlib import Path
 import os
+import dj_database_url
 from dotenv import load_dotenv
 load_dotenv()
 
@@ -15,14 +16,14 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-# SECRET_KEY = 'django-insecure-3$8#7tbi-2jm!x=w%-$)u!9y598j2g-p)s#7i0wi@z20ss6eoq'
+
 SECRET_KEY = os.getenv(
     "SECRET_KEY",
     "django-insecure-development-key"
 )
 
 # SECURITY WARNING: don't run with debug turned on in production!
-# DEBUG = True
+
 DEBUG = os.getenv("DEBUG", "False") == "True"
 
 ALLOWED_HOSTS = ["*"]
@@ -76,12 +77,8 @@ WSGI_APPLICATION = 'config.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
-import dj_database_url
+
 DATABASES = {
-    # 'default': {
-    #     'ENGINE': 'django.db.backends.sqlite3',
-    #     'NAME': BASE_DIR / 'db.sqlite3',
-    # }
     "default": dj_database_url.config(
         default=f"sqlite:///{BASE_DIR / 'db.sqlite3'}"
     )
